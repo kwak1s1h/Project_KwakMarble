0e728b8fbbe96b86d5b1b5eaf5a4b76481563291
kwak1s1h <kwak1s1h@kakao.com>
2024-04-15 11:48:09 +0900

    Auto-generate Packet Utill

---
+++ Server/Utill/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace Utill
+{
+    class Program
+    {
+        private const string ROOT_DIR = "D:\\Server\\Project_KwakMarble\\";
+
+        private const string CLIENT_PACKET_DIR = ROOT_DIR + "Server\\Network\\Packet\\Client";
+        private const string SERVER_PACKET_DIR = ROOT_DIR + "Server\\Network\\Packet\\Server";
+
+        private const string SERVER_OUT_DIR = ROOT_DIR + "Server\\Server\\PacketManager.cs";
+        private const string CLIENT_OUT_DIR = ROOT_DIR + "Client\\Assets\\01.Scripts\\Packet\\PacketManger.cs";
+
+        private const string TEMPLATE = @"using Network;
+using Network.Packet;
+using Network.Packet.Client;
+using Network.Packet.Server;
+
+namespace Server
+{
+    public class PacketManager
+    {
+        #region 싱글톤
+        // 패킷매니저는 수정할 일없으므로 싱글톤으로 간편히 유지
+        static PacketManager _instance = new PacketManager();
+        public static PacketManager Instance { get { return _instance; } }
+        #endregion
+
+        PacketManager()
+        {
+            Register();
+        }
+
+
+        Dictionary<ushort, Func<PacketSession, ArraySegment<byte>, IPacket>> _makeFunc
+            = new Dictionary<ushort, Func<PacketSession, ArraySegment<byte>, IPacket>>();
+        Dictionary<ushort, Action<PacketSession, IPacket>> _handler
+            = new Dictionary<ushort, Action<PacketSession, IPacket>>();
+
+        public void Register()
+        {
+            REGISTER_TEXT
+        }
+
+        public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer, Action<PacketSession, IPacket> onRecvCallback = null)
+        {
+            ushort count = 0;
+
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            count += 2;
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            Func<PacketSession, ArraySegment<byte>, IPacket> func = null;
+            if (_makeFunc.TryGetValue(id, out func))
+            {
+                IPacket packet = func.Invoke(session, buffer);
+                if (onRecvCallback != null)
+                    onRecvCallback.Invoke(session, packet);
+                else
+                    HandlePacket(session, packet);
+            }
+        }
+
+        T MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IPacket, new()
+        {
+            T pkt = new T();
+            pkt.Read(buffer);
+            return pkt;
+        }
+
+        public void HandlePacket(PacketSession session, IPacket packet)
+        {
+            Action<PacketSession, IPacket> action = null;
+            if (_handler.TryGetValue(packet.Protocol, out action))
+                action.Invoke(session, packet);
+        }
+    }
+}
+";
+
+        private const string REGISTER_TEMPLATE = @"_makeFunc.Add((ushort)PacketID.PACKET_NAME, MakePacket<PACKET_NAME>);
+            _handler.Add((ushort)PacketID.PACKET_NAME, PacketHandler.PACKET_NAMEHandler);
+
+            ";
+
+        public enum GenerateState
+        {
+            CLIENT,
+            SERVER,
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo clientPacketDir = new DirectoryInfo(CLIENT_PACKET_DIR);
+            string[] fileNames = Directory.GetFiles(CLIENT_PACKET_DIR, "*.cs");
+
+            string register = string.Empty;
+            for(int i = 0; i < fileNames.Length; i++) 
+            {
+                string fileName = fileNames[i];
+                if (File.Exists(fileName))
+                {
+                    string className = Path.GetFileNameWithoutExtension(fileName);
+                    register += REGISTER_TEMPLATE.Replace("PACKET_NAME", className);
+                }
+            }
+
+            string result = TEMPLATE.Replace("REGISTER_TEXT", register.TrimEnd());
+
+            if(File.Exists(SERVER_OUT_DIR))
+            {
+                File.WriteAllText(SERVER_OUT_DIR, result);
+            }
+        }
+    }
+}
