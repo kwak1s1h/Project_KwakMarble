487d51ae561dd50a45084180ce3f9517697aa2b8
kwak1s1h <kwak1s1h@kakao.com>
2024-04-15 12:56:16 +0900

    Client Packet Utill

---
+++ Client/Assets/01.Scripts/Network/Packet/PacketHandler.cs
@@ -0,0 +1,24 @@
+using Network;
+using Network.Packet;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacketHandler
+{
+    public static void S_EnterRoomHandler(PacketSession session, IPacket packet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void S_LeaveRoomHandler(PacketSession session, IPacket packet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void S_RoomInfoHandler(PacketSession session, IPacket packet)
+    {
+        throw new NotImplementedException();
+    }
+}
