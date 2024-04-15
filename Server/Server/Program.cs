e8fc9bd467c44452098d17cc3bf4c2d55aec1e44
kwak1s1h <kwak1s1h@kakao.com>
2024-04-15 09:15:32 +0900

    Network Project FIle

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,7 +1,17 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System.Net;
 
+namespace Server
+{
+    class Program
+    {
+        private const int _port = 8080;
+
+        static void Main(string[] args)
+        {
+            IPHostEntry iphost = Dns.GetHostEntry(Dns.GetHostName());
+            Console.WriteLine(iphost.AddressList[1]);
+            IPAddress ipAddr = iphost.AddressList[1];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, _port); // IP주소, 포트번호 입력
+        }
     }
 }
