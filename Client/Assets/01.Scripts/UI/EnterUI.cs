28b4f6da08123fc60c35a4e33a1c2968a3a8299a
hueam <zminsu5@gmail.com>
2024-04-26 09:26:11 +0900

    건물 건설 작업중

--- Client/Assets/01.Scripts/UI/EnterUI.cs
+++ Client/Assets/01.Scripts/UI/EnterUI.cs
@@ -7,11 +7,11 @@ public class EnterUI : UIBase
     public void CreateRoom()
     {
         //나 방만들거야
-        UIManager.Instance.GetUI<RoomUI>().gameObject.SetActive(true);
+        UIManager.Instance.GetUI<RoomUI>();
     }
     public void EnterRoom()
     {
-        UIManager.Instance.GetUI<RoomIdUI>().gameObject.SetActive(true);
+        UIManager.Instance.GetUI<RoomIdUI>();
     }
     public override void TurnOff()
     {
